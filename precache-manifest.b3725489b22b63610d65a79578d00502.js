1b7b8798611bdebd826c8d1109221df3f48e0939
f-adeniyi <holarfemilekan011@gmail.com>
2020-05-11 01:23:23 +0100

    Updates

---
+++ precache-manifest.b3725489b22b63610d65a79578d00502.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bb7d1b7e6ac1c31c4f3127c4ded08f7",
+    "url": "/world/index.html"
+  },
+  {
+    "revision": "a980d0d19fd078cc93ae",
+    "url": "/world/static/css/main.44db50aa.chunk.css"
+  },
+  {
+    "revision": "5e556ddaa92ccc9b2841",
+    "url": "/world/static/js/2.6970fc64.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/world/static/js/2.6970fc64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a980d0d19fd078cc93ae",
+    "url": "/world/static/js/main.71ddcfa2.chunk.js"
+  },
+  {
+    "revision": "4fdbb06a8130c8a933c4",
+    "url": "/world/static/js/runtime-main.72c821b9.js"
+  }
+]);
